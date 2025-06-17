066e5953b32bbb1a190b0e4444d843bb9d5bd839
Habibasubhani <subhani.habiba7860@gmail.com>
2025-06-17 11:44:14 +0530

    [habiba] added voting eligibilty code

---
+++ Day3_Arrays/VotingEligibility.java
@@ -0,0 +1,27 @@
+package LocalRepo.Day3_Arrays;
+
+import java.util.Scanner;
+
+public class VotingEligibility {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] ages = new int[10];
+
+        // Taking input for 10 students
+        for (int i = 0; i < ages.length; i++) {
+            System.out.print("Enter age of student " + (i + 1) + ": ");
+            ages[i] = sc.nextInt();
+        }
+
+        // Checking eligibility
+        for (int age : ages) {
+            if (age < 0) {
+                System.out.println("Invalid age");
+            } else if (age >= 18) {
+                System.out.println("The student with the age " + age + " can vote.");
+            } else {
+                System.out.println("The student with the age " + age + " cannot vote.");
+            }
+        }
+    }
+}
