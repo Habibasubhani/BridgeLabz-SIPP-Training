654bd3aebbdc4f739e27bf2a4b5959555e1ba3b4
Habibasubhani <subhani.habiba7860@gmail.com>
2025-07-16 12:28:25 +0530

    [habiba] add. Day3_SortingAlgorithm implement insertion sort in employee ids

---
+++ Day3_SortingAlgorithm/InsertionSortEmployeeIDs.java
@@ -0,0 +1,45 @@
+package LocalRepo.Day3_SortingAlgorithm;
+
+import java.util.Scanner;
+
+public class InsertionSortEmployeeIDs {
+
+    public static void insertionSort(int[] ids) {
+        int n = ids.length;
+
+        for (int i = 1; i < n; i++) {
+            int key = ids[i];
+            int j = i - 1;
+
+            // Move elements greater than key to one position ahead
+            while (j >= 0 && ids[j] > key) {
+                ids[j + 1] = ids[j];
+                j--;
+            }
+
+            ids[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of employees: ");
+        int n = sc.nextInt();
+
+        int[] ids = new int[n];
+        System.out.println("Enter " + n + " employee IDs:");
+        for (int i = 0; i < n; i++) {
+            ids[i] = sc.nextInt();
+        }
+
+        insertionSort(ids);
+
+        System.out.println("Sorted Employee IDs in ascending order:");
+        for (int id : ids) {
+            System.out.print(id + " ");
+        }
+
+        sc.close();
+    }
+}
