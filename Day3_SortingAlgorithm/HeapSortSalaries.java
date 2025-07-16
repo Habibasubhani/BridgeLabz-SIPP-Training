4f284dd8bb14e7d4221636428ea7ca65488e200e
Habibasubhani <subhani.habiba7860@gmail.com>
2025-07-16 12:34:21 +0530

    [habiba] add. Day3_SortingAlgorithm implemented heap sort on salaries

---
+++ Day3_SortingAlgorithm/HeapSortSalaries.java
@@ -0,0 +1,67 @@
+package LocalRepo.Day3_SortingAlgorithm;
+
+import java.util.Scanner;
+
+public class HeapSortSalaries {
+
+    public static void heapSort(int[] salaries) {
+        int n = salaries.length;
+
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(salaries, n, i);
+        }
+
+        for (int i = n - 1; i > 0; i--) {
+            int temp = salaries[0];
+            salaries[0] = salaries[i];
+            salaries[i] = temp;
+
+            heapify(salaries, i, 0);
+        }
+    }
+
+    public static void heapify(int[] arr, int n, int i) {
+        int largest = i;
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
+
+        if (l < n && arr[l] > arr[largest]) {
+            largest = l;
+        }
+
+        if (r < n && arr[r] > arr[largest]) {
+            largest = r;
+        }
+
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            heapify(arr, n, largest);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of applicants: ");
+        int n = sc.nextInt();
+        int[] salaries = new int[n];
+
+        System.out.println("Enter expected salaries:");
+        for (int i = 0; i < n; i++) {
+            salaries[i] = sc.nextInt();
+        }
+
+        heapSort(salaries);
+
+        System.out.println("Sorted salaries in ascending order:");
+        for (int salary : salaries) {
+            System.out.print(salary + " ");
+        }
+
+        sc.close();
+    }
+}
+
